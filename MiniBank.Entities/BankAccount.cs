255cdabc7a2e9a3bf0f87f34d5ee69a5b667d34d
Ajay0411943 <44640758+Ajay0411943@users.noreply.github.com>
2019-09-19 10:16:32 +0200

    Entities created

--- MiniBank.Entities/BankAccount.cs
+++ MiniBank.Entities/BankAccount.cs
@@ -6,5 +6,10 @@ namespace MiniBank.Entities
 {
     class BankAccount
     {
+        public int AccountNumber { get; set; }
+        public double InterestRate { get; set; }
+        public double ActualBalance { get; set; }
+        public List<Transaction> GetAllTransactions { get; }
+        public List<Customer> GetAllCusmtomers { get; }
     }
 }
